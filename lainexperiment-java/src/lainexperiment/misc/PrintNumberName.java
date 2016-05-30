57dd71f15abe1db29802ed7d4df43a0498684226
lain <lambdaflux@protonmail.com>
2016-05-30 11:43:20 +0200

    Print number name rework

--- lainexperiment-java/src/lainexperiment/misc/PrintNumberName.java
+++ lainexperiment-java/src/lainexperiment/misc/PrintNumberName.java
@@ -87,11 +87,51 @@ public class PrintNumberName {
         return s;
     }
 
+    static String print(int i, int base) {
+        if (i == 0) return "";
+        i--;
+        String[] nums = {
+            "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+        };
+        String[] tens = {
+            "twenty", "thirty", "fourty", "fivety", "sixty", "seventy", "eighty", "ninety"
+        };
+        if (base == 1) return nums[i] + " ";
+        if (base == 10) return tens[i - 1] + " ";
+        if (base == 100) return nums[i] + " hundred ";
+        return "";
+    }
+    
+    static String print(long n) {
+        String num = Long.toString(n);
+        int f = num.length() % 3;
+        if (f != 0)
+            num = String.format("%" + (3 - f) + "s", "").replace(' ', '0') + num;
+        String name = "";
+        String[] groups = {"", "thousand", "million", "billion"};
+        int g = 0;
+        for (int i = num.length() - 1; i > 0; i -= 3) {
+            String t = print(num.charAt(i - 2) - '0', 100);
+            if (num.charAt(i - 1) == '1') {
+                String[] teens = {
+                    "ten", "eleven", "twelve", "thirteen", "fourteen", "fiveteen", "sixteen", "seventeen", "eighteen", "nineteen"
+                };
+                t += teens[num.charAt(i) - '0'];
+            } else {
+                t += print(num.charAt(i - 1) - '0', 10);
+                t += print(num.charAt(i - 0) - '0', 1);
+            }
+            name = g == 0? t: t + groups[g] + " " + name;
+            g++;
+        }
+        return name.trim();
+    }
+    
     public static void main(String[] args) {
-        assertEquals("one thousand two hundred thirty four", name(1234));
-        assertEquals("one hundred ninety one million two hundred thirty two thousand eight hundred ninety one", name(191_232_891));
-        assertEquals("four hundred eight thousand one hundred sixty three", name(408_163L));
-        assertEquals("seven hundred eighty five billion six hundred fourty nine million four hundred eight thousand one hundred sixty three", name(785_649_408_163L));
+        assertEquals("one thousand two hundred thirty four", print(1234));
+        assertEquals("one hundred ninety one million two hundred thirty two thousand eight hundred ninety one", print(191_232_891));
+        assertEquals("four hundred eight thousand one hundred sixty three", print(408_163L));
+        assertEquals("seven hundred eighty five billion six hundred fourty nine million four hundred eight thousand one hundred sixty three", print(785_649_408_163L));
     }
 
 }
