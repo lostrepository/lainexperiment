4c88bcaff4d3430e136382bcc68ac19a339d8f7b
lain <lambdaflux@protonmail.com>
2016-11-01 22:40:05 +0100

    Improving deep iterator

--- lainexperiment-java/src/lainexperiment/misc/DeepIterator.java
+++ lainexperiment-java/src/lainexperiment/misc/DeepIterator.java
@@ -27,9 +27,8 @@ package lainexperiment.misc;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.function.Supplier;
+import java.util.Stack;
 import java.util.stream.Stream;
 
 public class DeepIterator {
@@ -37,73 +36,73 @@ public class DeepIterator {
     @SuppressWarnings({"rawtypes"})
     static class DIterator {
        
-        List<Iterator> iters = new LinkedList<>();
-        Supplier<Iterator> tail = () -> iters.get(iters.size() - 1);
-        Object next;
+        Stack<Iterator> q = new Stack<>();
+        Object val;
         
         DIterator(List list) {
-            iters.add(list.iterator());
-            next = expandTail();
+            q.add(list.iterator());
+            expand();
         }
         
         // immutable operation
         boolean hasNext() {
-            return next != null;
+            return val != null;
         }
 
         Object next() {
-            if (next == null) return null;
-            Object cur = next;
-            if (tail.get().hasNext())
-                next = tail.get().next();
-            else
-                next = update();
-            return cur;
+            if (q.isEmpty()) return null;
+            Object ret = val;
+            if (q.peek().hasNext()) {
+                val = q.peek().next();
+                return ret;
+            }
+            expand();
+            return ret;
         }
 
-        private Object update() {
-            if (iters.isEmpty()) return null;
-            if (tail.get().hasNext()) {
-                return expandTail();
+        private void expand() {
+            while (!q.isEmpty()) {
+                if (!q.peek().hasNext()) {
+                    q.pop();
+                    continue;
+                }
+                Object v = q.peek().next();
+                if (!(v instanceof List)) {
+                    val = v;
+                    break;
+                }
+                q.add(((List) v).iterator());
             }
-            iters.remove(iters.size() - 1);
-            return update();
-        }
-        
-        private Object expandTail() {
-            while (tail.get().hasNext()) {
-                Object obj = tail.get().next();
-                if (obj instanceof List)
-                    iters.add(((List)obj).iterator());
-                else
-                    return obj;
-            }
-            return null;
         }
     }
     
     static int c = 0;
     
+    /*
+     * Creates a list of lists of depth d.
+     * On each level will be only 3 entities.
+     */
     @SuppressWarnings("unchecked")
-    static List<?> makeNestedList(int i, int n) {
-        if (i == n) {
+    static List<?> makeNestedList(int i/*cur depth*/, int d) {
+        if (i == d) {
             Integer[] a = new Integer[]{c + 1, c + 2, c + 3};
             c += 3;
             return Arrays.asList(a);
         }
         @SuppressWarnings("rawtypes")
         List l = new ArrayList<>();
-        l.add(makeNestedList(i + 1, n));
-        l.add(makeNestedList(i + 1, n));
-        l.add(makeNestedList(i + 1, n));
+        l.add(makeNestedList(i + 1, d));
+        l.add(makeNestedList(i + 1, d));
+        l.add(makeNestedList(i + 1, d));
         return l;
     }
     
     public static void main(String[] args) {
-        List<?> l = makeNestedList(0, 2);
         DIterator di = new DIterator(Arrays.asList(new Integer[]{1, 2, 3}));
         di = new DIterator(Arrays.asList(new Integer[]{1, 2, 3}));
         Stream.generate(di::next).peek(System.out::println).anyMatch(v -> v == null);
+
+        List<?> l = makeNestedList(0, 2);
         di = new DIterator(l);
         Stream.generate(di::next).peek(System.out::println).anyMatch(v -> v == null);
     }
