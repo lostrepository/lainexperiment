3822b35b6cc0c44cf1f464a8ef29d8cd1d9e943f
lain <lambdaflux@protonmail.com>
2019-10-21 22:21:11 -0700

    Threads ordering

---
+++ lainexperiment-java/src/lainexperiment/misc/Threads_ordering.java
@@ -0,0 +1,69 @@
+/*
+ * Date: 21/10/2019
+ * 
+ * Problem
+ * 
+ * There are methods m1, m2, m3 and 3 threads each calling its own method.
+ * Make sure that threads can run methods in the following order:
+ * 
+ * 1. Thread 1 runs m1
+ * 2. Thread 2 runs m2
+ * 3. Thread 3 runs m3
+ *
+ * Output
+ * 
+123
+123
+123
+...
+ * 
+ */
+package lainexperiment.misc;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Semaphore;
+
+public class Threads_ordering {
+
+    Semaphore s2 = new Semaphore(1);
+    Semaphore s3 = new Semaphore(1);
+
+    void m1() {
+        System.out.print("1");
+        s2.release();
+    }
+
+    void m2() {
+        try {
+            s2.acquire();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.print("2");
+        s3.release();
+    }
+
+    void m3() {
+        try {
+            s3.acquire();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println("3");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        var v = new Threads_ordering();
+        v.s2.acquire();
+        v.s3.acquire();
+        while (true) {
+            ForkJoinPool.commonPool().submit(() -> v.m2());
+            ForkJoinPool.commonPool().submit(() -> v.m1());
+            ForkJoinPool.commonPool().submit(() -> v.m3());
+            Thread.sleep(100);
+        }
+    }
+
+}
---
+++ lainexperiment-java/src/module-info.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+/**
+ * @author lynx
+ *
+ */
+module lainexperiment {
+    exports lainexperiment.devdraft.atlascodechallenge;
+    exports lainexperiment.hackerrank.weekofcode._17;
+    exports lainexperiment.codejam.qualification._2008;
+    exports lainexperiment.codejam.round._1b._2008;
+    exports lainexperiment.codejam.round._1c._2017;
+    exports lainexperiment.hackerrank.codesprint.zalando;
+    exports lainexperiment.mensikovbook;
+    exports lainexperiment.hackerrank.hackathon.countercode2015;
+    exports lainexperiment.hackerrank.weekofcode._12;
+    exports lainexperiment.hackerrank.weekofcode._34;
+    exports lainexperiment.codility.tomtom;
+    exports lainexperiment.hackerrank.hack101._2016.nov;
+    exports lainexperiment.hackerrank.worldcodesprint._6;
+    exports lainexperiment.hackerrank.worldcodesprint._5;
+    exports lainexperiment.codejam._2010.round._1a;
+    exports lainexperiment.hackerrank.worldcodesprint._8;
+    exports lainexperiment.hackerrank.hackathon.cisco;
+    exports lainexperiment.hackerrank.worldcodesprint._7;
+    exports lainexperiment.codility.imdb;
+    exports lainexperiment.codility.weaveworks;
+    exports lainexperiment.codility.po;
+    exports lainexperiment.hackerrank.amazon;
+    exports lainexperiment.hackerrank.worldcodesprint._4;
+    exports lainexperiment.misc;
+    exports lainexperiment.hackerrank.hack101._2015.nov;
+    exports lainexperiment.hackerrank.weekofcode._1;
+    exports lainexperiment.hackerrank.weekofcode._25;
+    exports lainexperiment.hackerrank.codesprint.blackrock;
+    exports lainexperiment.hackerrank.hack101._2016.jun;
+    exports lainexperiment.hackerrank.misc;
+    exports lainexperiment.hackerrank.weekofcode._27;
+    exports lainexperiment.hackerrank.weekofcode._26;
+    exports lainexperiment.codejam.qualification._2017;
+    exports lainexperiment.hackerrank.weekofcode._28;
+    exports lainexperiment.codejam.round._1b._2017;
+    exports lainexperiment.codility.zalando;
+    exports lainexperiment.codejam._2008.round._1c;
+    exports lainexperiment.hackerrank.codesprint.bookingcom;
+    exports lainexperiment.hackerrank.hackathon.bookingcom;
+    exports lainexperiment.hackerrank.weekofcode._23;
+    exports lainexperiment.hackerrank.weekofcode._22;
+    exports lainexperiment.codejam.round._1a._2017;
+    exports lainexperiment.hackerrank.hack101._2016.aug;
+    exports lainexperiment.codejam.practiceproblems;
+    exports lainexperiment.hackerrank.hackathon.newyorklife;
+    exports lainexperiment.codejam._2011.round._1b;
+    exports lainexperiment.codility.cimpress;
+
+    requires java.desktop;
+    requires junit;
+}
