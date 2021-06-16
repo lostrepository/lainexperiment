989e17d8c2f28842a480621a5ecfad01f564f2aa
lain <lambdaflux@protonmail.com>
2021-06-16 03:08:58 +0000

    Implementing PairInt instead of relying on Pair

--- lainexperiment-java/src/lainexperiment/utils/pairs/PairInt.java
+++ lainexperiment-java/src/lainexperiment/utils/pairs/PairInt.java
@@ -6,25 +6,68 @@
  */
 package lainexperiment.utils.pairs;
 
+import java.util.Objects;
+
 /**
- * Short name for {@code Pair<Integer, Integer> }
+ * Unfortunately {@code Pair<Integer, Integer> } is not an
+ * option because it would not be safe to compare elements of
+ * two different pairs with == instead you will need to use equals().
  */
-public class PairInt extends Pair<Integer, Integer> {
+public class PairInt {
+
+    public int a;
+    public int b;
+
+    public PairInt() {
+    }
 
     public PairInt(int a, int b) {
-        super(a, b);
+        this.a = a; this.b = b;
     }
     
     public PairInt(PairInt p) {
-        super(p.a, p.b);
-    }
-
-    public PairInt() {
-        super(0, 0);
+        this.a = p.a; this.b = p.b;
     }
     
+    public int getA() {
+        return a;
+    }
+    
+    public int getB() {
+        return b;
+    }
+    
+    public static int compareByA(PairInt p1, PairInt p2) {
+        return p1.a - p2.a;
+    }
+    
+    public static int compareByB(PairInt p1, PairInt p2) {
+        return p1.b - p2.b;
+    }
+
     public static int compareByAB(PairInt p1, PairInt p2) {
-        int r = p1.a - p2.a;
-        return (r == 0)? p1.b - p2.b: r;
+        int r = compareByA(p1, p2);
+        return (r == 0)?  compareByB(p1, p2): r;
+    }
+    
+    public static int reverseCompareByAB(PairInt p1, PairInt p2) {
+        return -compareByAB(p1, p2);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b);
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (obj.getClass() != getClass()) return false;
+        PairInt o = (PairInt) obj;
+        return a == o.a && b == o.b;
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("(%d, %d)", a, b);
     }
 }
